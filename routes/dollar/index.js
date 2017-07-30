ff33de4a5c75fd656f2690b6df386d5d96f86756
Andres Zorro <andres.zorro@zemoga.com>
2017-07-30 13:34:05 -0500

    Finish API

--- routes/dollar/index.js
+++ routes/dollar/index.js
@@ -4,14 +4,55 @@
  */
 
 const logger = require('debug-logger')('scrapper');
+const fetch = require('node-fetch');
+const moment = require('moment');
+const cheerio = require('cheerio');
 const SCRAPE_URL = 'https://dolar.wilkinsonpc.com.co/dolar-historico/dolar-historico-{{year}}.html';
+
+moment.locale('es');
 
 module.exports = {
     params: '',
-    fn(req, res) {
-        res.send({
-            date: req.query.date,
-            USD: 1234
-        });
+    fn: async function (req, res) {
+        try {
+            const date = moment(req.query.date || undefined);
+
+            if (!date.isValid()) {
+                const err = new Error('Invalid date');
+                err.statusCode = 400;
+                throw err;
+            }
+
+            const url = SCRAPE_URL.replace('{{year}}', date.year());
+
+            logger.log('Scraping:', url);
+
+            let $ = await fetch(url);
+            $ = await $.text();
+            $ = cheerio.load($);
+
+            let dias = $('#tabla_dh .dh_col_fecha').map(function () {
+                const $dia = $(this);
+                const fecha = $dia.text();
+                const precio = $dia.next().text();
+
+                return {
+                    date: moment(fecha.toLowerCase(), 'DD MMMM YYYY'),
+                    value: Number.parseFloat(precio.replace(/[$,]*/g, '').trim())
+                };
+            });
+
+            dias = Array.from(dias);
+            let trm = dias.find((d) => d.date.isSame(date, 'days'));
+
+            logger.log('Found date?', trm);
+
+            res.send(trm);
+        } catch (err) {
+            res.send(err.statusCode || 500, {
+                message: err.message || 'Internal Server Error',
+                stack: err.stack
+            });
+        }
     }
 };
